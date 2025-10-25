409da0f9d77670e46204fd418b3efdc3129902a5
marvinmaerz <58120263+marvinmaerz@users.noreply.github.com>
2025-10-25 11:43:49 +0200

    Finished exercise sheet 0 (task 2)

--- Exercise 0 Numpy/src_to_implement/generator.py
+++ Exercise 0 Numpy/src_to_implement/generator.py
@@ -1,6 +1,7 @@
 import os.path
 import json
 import scipy.misc
+import skimage
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -41,7 +42,8 @@ class ImageGenerator:
         self.epoch = 0
         self.current_batch = 0
         self.batches_per_epoch = len(self.images) / self.batch_size
-        self.indices = np.arange(len(self.images))          # shuffle changes this array # TODO: if shuffle=True, change this array
+        self.indices = np.arange(len(self.images))          # shuffle changes this array, since it dictates accesses to images and labels
+        if self.shuffle: self.shuffle_indices()
 
 
     # This function creates a batch of images and corresponding labels and returns them.
@@ -49,25 +51,33 @@ class ImageGenerator:
     # Note that your amount of total data might not be divisible without remainder with the batch_size.
     # Think about how to handle such cases
     def next(self):
-        #TODO: add resize boolean option
+        images = np.empty((self.batch_size, self.image_size[0], self.image_size[1], 3))
+        labels = np.empty(self.batch_size, dtype=int)
+
         if self.current_batch == self.batches_per_epoch:         # increase epoch
             self.epoch += 1
             self.current_batch = 0
-            # TODO: implement shuffling after an epoch
+            if self.shuffle: self.shuffle_indices()
 
-        images = []
-        labels = []
-        # get indices to access for this batch. If shuffle=False, this iterates over the images in-order
-        batch_indices = self.indices[self.batch_size * self.current_batch : self.batch_size * (self.current_batch + 1)]
+        # Resizing images
+        resize = self.image_size != self.images[0].shape      # resize = True, iff image_size not equal to the test data size
+        if resize:
+            for i in range(len(self.images)):
+                self.images[i] = skimage.transform.resize(self.images[i], self.image_size)
+
+        # get indices to access for this batch. If shuffle=False, this iterates over the images in-order, else accesses given indices for each batch (random order)
+        batch_indices = self.indices[self.batch_size * self.current_batch: self.batch_size * (self.current_batch + 1)]
 
         # fill in batch_indices, if the last batch in the epoch was not filled completely => take images from the front
         remainder = self.batch_size - len(batch_indices)
         for i in range(remainder):
             batch_indices = np.append(batch_indices, i)
 
+        i = 0   # fill images and labels from 0 -- batch_size, but access self.images via idx from batch_indices
         for idx in batch_indices:
-            images.append(np.copy(self.images[idx]))
-            labels.append(self.labels[self.image_names[idx]])
+            images[i] = self.augment(np.copy(self.images[idx]))
+            labels[i] = self.labels[self.image_names[idx]]
+            i += 1
 
         self.current_batch += 1
 
@@ -77,9 +87,27 @@ class ImageGenerator:
     # this function takes a single image as an input and performs a random transformation
     # (mirroring and/or rotation) on it and outputs the transformed image
     def augment(self,img):
-        #TODO: implement augmentation function: mirroring & rotating
+        if not self.rotation and not self.mirroring: return img
+        rng = np.random.default_rng()
+        augment = rng.integers(0, 1, endpoint=True)         # to augment or not to augment, that is the question
+        if not augment: return img
 
-        return img
+        img_aug = None
+        if self.rotation:
+            degree = rng.choice([90, 180, 270])
+            img_aug = skimage.transform.rotate(img, degree)
+
+        if self.mirroring:
+            direction = rng.integers(0, 1, endpoint=True)
+            img_aug = np.fliplr(img) if direction == 0 else np.flipud(img)
+
+        return img_aug
+
+
+    # shuffles indices for the next epoch (helper method)
+    def shuffle_indices(self):
+        rng = np.random.default_rng()
+        rng.shuffle(self.indices)
 
 
     def current_epoch(self):
@@ -95,7 +123,7 @@ class ImageGenerator:
     # batch of images and labels and visualizes it.
     def show(self):
         images, labels = self.next()
-        plt.figure(figsize=(10, 10))        # tip: use height 20 for big batches (60+ images), and 10 for around 20-30 images
+        plt.figure(figsize=(10, 10))        # tip: use height 20 (second value) for big batches (60+ images), and 10 for around 20-30 images
         for i in range(len(images)):
             plt.subplot(int(len(images)/5),5,i+1)
             plt.axis('off')
