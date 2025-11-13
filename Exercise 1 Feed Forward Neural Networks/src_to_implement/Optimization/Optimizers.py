73a09a002a5d1c76b8eb90b90cd321d11ab50fe1
marvinmaerz <58120263+marvinmaerz@users.noreply.github.com>
2025-11-13 15:52:00 +0100

    Added documentation.

--- Exercise 1 Feed Forward Neural Networks/src_to_implement/Optimization/Optimizers.py
+++ Exercise 1 Feed Forward Neural Networks/src_to_implement/Optimization/Optimizers.py
@@ -2,13 +2,20 @@ import numpy as np
 
 
 
-# Basic Stochastic Gradient Descent (SGD) class.
-# Updates weights based on the basic gradient update scheme.
 class Sgd:
+    """
+    Basic Stochastic Gradient Descent (SGD) class.
+    Updates weights based on the basic update scheme.
+    """
     def __init__(self, learning_rate:float):
         self.learning_rate = learning_rate
 
-    # Basic sgd weight update rule.
-    # Returns the updated weight tensor.
+
     def calculate_update(self, weight_tensor, gradient_tensor):
+        """
+        Basic stochastic gradient descent weight update rule.
+        :param weight_tensor: weight matrix of any layer.
+        :param gradient_tensor: gradient of the loss function with respect to the weights.
+        :return: the updated weight tensor.
+        """
         return weight_tensor - self.learning_rate * gradient_tensor
