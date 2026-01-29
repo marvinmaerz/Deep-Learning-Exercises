e428e27536e5de609cc6feeac13d30af7efad9f7
marvinmaerz <58120263+marvinmaerz@users.noreply.github.com>
2026-01-30 00:48:00 +0100

    Custom dataset class semi-finished
    Semi-finished since I probably need to slightly adjust the transforms when tuning the model and hyperparamters

--- Exercise 4 PyTorch Challenge/src_to_implement/data.py
+++ Exercise 4 PyTorch Challenge/src_to_implement/data.py
@@ -11,5 +11,69 @@ train_std = [0.16043035, 0.16043035, 0.16043035]
 
 
 class ChallengeDataset(Dataset):
-    # TODO implement the Dataset class according to the description
-    pass
+    def __init__(self, data, mode: str):
+        """
+        Arguments:
+            data (pandas.dataframe): Holds the data found in "data.csv"
+            mode (string): Flag with possible values "val" or "train"
+        """
+        self.data = data
+        self.mode = mode
+
+        # Perform data augmentation on training set, normalize
+        self.train_transform = tv.transforms.Compose([
+            tv.transforms.ToPILImage(),
+
+            # Data augmentation transformations:
+            tv.transforms.RandomResizedCrop(300),
+            tv.transforms.RandomHorizontalFlip(),
+            tv.transforms.RandomVerticalFlip(),
+            tv.transforms.RandomRotation(10),
+            tv.transforms.ColorJitter(brightness=0.1, contrast=0.1),
+
+            tv.transforms.ToTensor(),
+            tv.transforms.Normalize(train_mean, train_std)
+        ])
+
+        # On validation set only normalize
+        self.validation_transform = tv.transforms.Compose([
+            tv.transforms.ToPILImage(),
+            tv.transforms.ToTensor(),
+            tv.transforms.Normalize(train_mean, train_std)
+        ])
+
+        self.dirname = Path(__file__).parent.resolve()      # should be something like WindowsPath("C:/.../repository/<exercise folder>/src_to_implement")
+
+        # Loading images into memory (only ~70 MB), hoping for faster read times at the cost of higher initial overhead.
+        # Toggle via boolean below.
+        # Results: test_normalization runtime using self.images in memory: ~6,5 seconds; with on demand loading: ~10,5 seconds
+        # => ~40% faster access times
+        self.preload_images = False
+        if self.preload_images:
+            self.images : dict[str, np.ndarray] = {}        # Example key-value pair: { "images/cell0001.png": [1 (cracked), 0 (inactive)] }
+            for i in range(data.shape[0]):
+                img_path = self.data["filename"][i]
+                img_path_full = self.dirname.joinpath(img_path)
+                img = imread(img_path_full)
+                self.images[img_path] = img
+
+
+    def __len__(self):
+        return self.data.shape[0]
+
+
+    def __getitem__(self, index):
+        img_path, cracked, inactive = self.data.iloc[index]
+        if self.preload_images:
+            img = self.images[img_path]                                 # faster reading of pre-loaded images in working memory
+        else:
+            img_path_full = self.dirname.joinpath(img_path)
+            img = imread(img_path_full)         # on demand reading from external memory (may be slower)
+        img_rgb = gray2rgb(img)
+        label = torch.tensor([cracked, inactive])
+        if self.mode == "train":
+            return self.train_transform(img_rgb), label
+        elif self.mode == "val":
+            return self.validation_transform(img_rgb), label
+        else: return img_rgb, label
+
